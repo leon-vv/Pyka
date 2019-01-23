5e68ab78356723e19530b09f82ec2644cd2911de
Léon van Velzen <leonvanvelzen@protonmail.com>
2019-01-23 20:32:27 +0100

    Fix old test code, add tests for apply

--- test/bootstrap-test.py
+++ test/bootstrap-test.py
@@ -37,26 +37,26 @@ class SchemeTest(unittest.TestCase):
                 "'(some \"list\")",
                 [Symbol('quote'), [Symbol('some'), 'list']])
     
-    def str_test(self, data, s):
-        self.assertEqual(str(data), s)
+    def repr_test(self, data, s):
+        self.assertEqual(scheme_repr(data), s)
     
     def test_str(self):
-        self.str_test(Symbol('abc'), 'abc')
-        self.str_test(String('abc'), "'abc'")
-        self.str_test(emptyList, '()')
-        self.str_test(Cons(1, 2), '(1 . 2)')
-        self.str_test(Cons(1, emptyList), '(1)')
-        self.str_test(Cons(1, Cons(2, emptyList)), '(1 2)')
-        self.str_test(Vector([1,2]), '#(1 2)')
+        self.repr_test(Symbol('abc'), 'abc')
+        self.repr_test('abc', '"abc"')
+        self.repr_test(emptyList, '()')
+        self.repr_test(Cons(1, 2), '(1 . 2)')
+        self.repr_test(Cons(1, emptyList), '(1)')
+        self.repr_test(Cons(1, Cons(2, emptyList)), '(1 2)')
+        self.repr_test([1,2], '#(1 2)')
     
     def test_eval_all(self):
        self.assertEqual(
-               eval_all(global_env,
+               eval_all(new_global_env(),
                    datum.parse_strict('((+ 10 20) (+ 20 30))')),
                Cons(30.0, Cons(50.0, emptyList)))
 
     def eval_test(self, code, result):
-        self.assertEqual(eval_string(global_env, code), result)
+        self.assertEqual(eval_string(code), result)
 
     def test_plus(self):
         self.eval_test('(+ 10 20)', 30)
@@ -66,6 +66,12 @@ class SchemeTest(unittest.TestCase):
 
     def test_vararg(self):
         self.eval_test('((dyn-lambda x x) 1 2)', [1, 2])
+
+    def test_apply(self):
+        self.eval_test("(apply + '(1 2 3))", 6)
+        self.eval_test("(apply + '())", 0)
+        self.eval_test("(apply length '( (1 2 3) ))", 3)
+
  
 if __name__ == '__main__':
     unittest.main()
