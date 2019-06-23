dff29d1ff89461696cb752c6f8bb6754f055a43c
Léon van Velzen <leonvanvelzen@protonmail.com>
2019-06-23 15:35:10 +0200

    Fix multiple bugs in iter, add a few tests

--- src/iter.lisp
+++ src/iter.lisp
@@ -1,7 +1,7 @@
 (load "src/match.lisp")
 
 (def-d-fexpr hash-table-set-symbol! (ht sym)
-  (hash-table-set! ht sym (eval-prev sym)))
+  (hash-table-set! (eval-prev ht) sym (eval-prev sym)))
   
 ; A 'generator' is a function that returns the next
 ; value of a sequence at each invocation. When there
@@ -22,11 +22,11 @@
   2)
 
 (assert-equal
-  (lets g (make-list-gen '())
-    (with/cc abc (g abc) 10)
-    5)
-  5)
-
+  (let ((x 5) (g (make-list-gen '())))
+    (g (l-fun () (set! x 15)))
+    x)
+  15)
+    
 (def-d-fun compute-step (keyword start . by)
   (lets
       direction
@@ -61,7 +61,6 @@
     (g #f) (g #f) (g #f) (g #f) (g (d-fun () #f)))
   #f)
 
-#|
 (def-d-fexpr iter clauses
  
   (if (null? clauses) (do () (#f))) ; Infinite loop
@@ -70,75 +69,99 @@
     ((base-env (env-ref 2))
      (control-flow-ht (make-hash-table)) ; Will contain 'finish', 'leave', 'next', etc.
      
-     (initially #f) ; Code placement
-     (finally #f)
+     (initially '()) ; Code placement
+     (finally '())
       
      (gen-ht (make-hash-table)) ; Generator hash table used by 'next' function
      (is-for (make-hash-table)) ; Which of the generators is produced by a for statement
       
      (repeat #f) ; Wheter there is a bound on the loop repetitions
-     (code '()) ; User code (i.e. non driver code)
+     (code '())) ; User code (i.e. non driver code)
      
-    (do ((c (car clauses) (cdr c)))
+   (def-d-fun set-is-for  ()
+      (if (equal? (car (car c)) 'for)
+        (hash-table-set! is-for var #f)))
+  
+   (do ((c clauses (cdr c)))
         ((null? c) #f)
-      
-     (match c
+     (match (car c)
        (`(repeat ,times)
         (if (equal? repeat #f)
           (set! repeat times)
           (set! repeat (min times repeat))))
           
-       (`(for ,var in ,list)
-        (hash-table-set! gen-ht var (list-gen (eval list base-env))))
-      
-       ((list (or 'for 'generating') var seq-kw start)
+       (`(,(or 'for 'generating) ,var in ,list)
+        (hash-table-set! gen-ht var (make-list-gen (eval list base-env)))
+        (set-is-for))
+        
+       ((list (or 'for 'generating) var seq-kw start)
         (hash-table-set! gen-ht var (numer-gen start (compute-step seq-kw start) #f))
-        (if (equal? (car c) 'for) (hash-table-set! for-ht var #f)))
+        (set-is-for))
        
        ((list (or 'for 'generating) var seq-kw start 'by by)
         (hash-table-set! gen-ht var (numer-gen start (compute-step seq-kw start by) #f))
-        (if (equal? (car c) 'for) (hash-table-set! for-ht var #f)))
+        (set-is-for))
 
-       ((list (or 'for 'generating') var seq-kw1 start seq-kw2 end)
+       ((list (or 'for 'generating) var seq-kw1 start seq-kw2 end)
         (hash-table-set! gen-ht var
           (numer-gen start (compute-step seq-kw start)
                            (compute-end seq-kw2 end)))
-        (if (equal? (car c) 'for) (hash-table-set! for-ht var #f)))
+        (set-is-for))
       
-        ((list-rest 'initially clauses)
+       ((list-rest 'initially clauses)
           (set! initially (append initially clauses)))
-        ((list-rest 'finally clauses)
-          (set! finally (append initally finally)))
-        ((list-rest 'after-each clauses)
-          (set! after-each (append after-each clauses)))
+       ((list-rest 'finally clauses)
+          (set! finally (append finally clauses)))
         
-        (c (set! code (append code c)))))
+       (c (set! code (append code c)))))
 
     (with/cc leave
-      (with/cc finish
-        
-          (hash-table-set-symbol! control-flow-ht leave)
-          (hash-table-set-symbol! control-flow-ht finish)
-          (hash-table-set! control-flow-ht
-                          'next
-                          (l-fun g 
-                            (if (null? g) ; Handle repeat
-                              (lets r repeat (set! repeat (- repeat 1)) r)
-                              (hash-table-ref gen-ht g) finish)))
-           
+      (with/cc finish-with
+          
+          (lets finish (l-fun () (finish-with #f))
+            (hash-table-set-symbol! control-flow-ht leave)
+            (hash-table-set-symbol! control-flow-ht finish)
+            (hash-table-set! control-flow-ht
+                            'next
+                            (l-fexpr args
+                              (if (null? args) ; Handle repeat
+                                (lets r repeat (set! repeat (- repeat 1)) (equal? r 0))
+                                ((hash-table-ref gen-ht (car args)) finish)))))
           (eval
-            (begin
-                ,initially
+            `(begin
+                ,@initially
                 (do ,(map (d-fun (k)
-                              (if (hash-table-exists? for-ht k)
+                              (if (hash-table-exists? is-for k)
                                 `(,k (next ,k) (next ,k))
-                                `(,k (next ,k)))) (hash-table-keys gen-ht))
-                  ,(if repeat '((next)) #f)
-                  ,code)))
-            (cons control-flow-ht base-env))))
+                                `(,k (next ,k))))
+                          (hash-table-keys gen-ht))
+                  ,(if repeat '((next)) '(#f))
+                  ,code))
+              (cons control-flow-ht base-env)))
       
-      (eval final-code base-env))
+      (eval-all finally base-env))))
 
-|#
+(assert-equal
+  (iter (for x in '(1 2 3)) (if (equal? x 2) (leave x)))
+  2)
 
- 
+(assert-equal
+  (iter (generating x in '(1 2 3))
+    (leave (next x)))
+  2)
+
+(assert-equal
+  (lets x 3
+    (iter (repeat 3) (set! x (+ x 1)))
+    x)
+  6)
+
+(assert-equal
+  (lets x 5
+    (iter (for y in '(1 2 3 4 5))
+      (initially (set! x 10))
+      (if (equal? y 3) (finish))
+      (finally x)))
+  10)
+
+
