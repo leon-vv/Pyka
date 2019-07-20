87256b7acb21a238e98b088f42eaa9ca1580d635
Léon van Velzen <leonvanvelzen@protonmail.com>
2019-07-20 14:53:38 +0200

    Use environments to keep track of bindings in match.lisp

--- src/match.lisp
+++ src/match.lisp
@@ -1,10 +1,5 @@
 (load "src/base.lisp")
 
-(def-macro loop cmnds
-  `(do ()
-       (#f)
-       ,@cmnds))
-
 (def-d-fun literal? (val)
   (or (boolean? val)
       (string? val)
@@ -29,127 +24,133 @@
 (assert-equal (parse-repeater '__abc) #f)
 
 ; Let's define a 'matcher' as a function which
-; takes a value to match, a pattern and a hash table.
+; takes a value to match, a pattern and an environment.
 ; A matcher should return #f if the pattern fails to match
-; and - this is important - not mutate the passed hash table.
+; and not mutate the passed environment.
 ; If the passed value does match the pattern however, the matcher
-; should return a hash table which possibly contains new matched
-; values. It's also allowed in this case to update
-; the passed hash table and simply return it.
+; should return an environment with new bindings.
+; Note that this means that a matcher that matches successfully but
+; does not create bindings returns the empty list (which in Scheme
+; is not the same as #f).
 
-(def-d-fun match-qp (val pat ht)
+; Unsuitable other definitions of a 'matcher':
+; Just passing in a hash table and letting the matcher
+; mutate it doesn't work with patterns of the fornm
+; (list a ...)
+; Just returning the new bindings without passing in anything
+; doesn't work with
+; (not a b c)
+
+(def-d-fun match-qp (val pat env)
   (cond
     ((literal? pat)
-      (match-literal val pat ht))
+      (match-literal val pat env))
     ((symbol? pat)
-      (if (equal? val pat) ht #f))
+      (if (equal? val pat) '() #f))
     ((and (list? pat) (not (null? pat)))
       (if (equal? (car pat) 'unquote)
-        (match-pat val (car (cdr pat)) ht)
-        (match-list val pat ht match-qp)))))
+        (match-pat val (car (cdr pat)) env)
+        (match-list val pat env match-qp)))))
 
-(def-d-fun match-list (val pats ht matcher)
+(def-d-fun match-list (val pats env matcher)
   (if (and (null? val) (null? pats))
-    ht
+    '()
     (if (or (null? val) (null? pats))
       #f 
-      (lets res (matcher (car val) (car pats) (hash-table-copy ht))
-        (if (hash-table? res)
-          (match-list (cdr val) (cdr pats) res matcher)
-          #f)))))
+      (lets bind (matcher (car val) (car pats) env)
+        (and bind
+          (lets other-bind (match-list (cdr val) (cdr pats) (append bind env) matcher)
+            (and other-bind (append bind other-bind))))))))
 
-(def-d-fun match-list-rest (val pats ht)
+(def-d-fun match-list-rest (val pats env)
   (cond
     ((or (null? val) (null? pats)) #f)
     ((null? (cdr pats))
-      (match-pat val (car pats) ht))
+      (match-pat val (car pats) env))
     (else
-      (lets res (match-pat (car val) (car pats) ht)
-        (if (hash-table? res)
-          (match-list-rest (cdr val) (cdr pats) res)
-          #f)))))
+      (lets bind (match-pat (car val) (car pats) env)
+        (and bind
+          (lets other-bind (match-list-rest (cdr val) (cdr pats) (append bind env))
+            (and other-bind (append bind other-bind))))))))
 
-(def-d-fun match-cons (val pat ht)
-  (lets res (match-pat (car val) (car pat) (hash-table-copy ht))
-    (if (hash-table? res)
-      (match-pat (cdr val) (car (cdr pat)) res)
-      #f)))
+(def-d-fun match-cons (val pat env)
+  (lets res (match-pat (car val) (car pat) env)
+    (and res
+      (lets other (match-pat (cdr val) (car (cdr pat)) (append res env))
+        (and other (append res other))))))
 
-(def-d-fun match-literal (val pat ht)
-  (if (equal? pat val) ht #f))
+(def-d-fun match-literal (val pat env)
+  (if (equal? pat val) '() #f))
 
-(def-d-fun match-symbol (val pat ht)
-  (if (hash-table-exists? ht pat)
-    (if (equal? (hash-table-ref ht pat) val) ht #f)
-    (begin
-      (hash-table-set! ht pat val)
-      ht)))
-
-(def-d-fun match-pat (val pat ht)
+(def-d-fun match-pat (val pat env)
   (cond
-    ((equal? pat '_) ht)
+    ((equal? pat '_) '())
     
-    ((symbol? pat) (match-symbol val pat ht))
-   
-    ((literal? pat) (match-literal val pat ht))
-    
+    ((symbol? pat)
+      (if (env-exists? env pat)
+        (if (equal? (env-ref env pat) val) '() #f)
+        (lets h (make-hash-table)
+          (hash-table-set! h pat val)
+          (list h))))
+     
+    ((literal? pat)
+      (match-literal val pat env))
+     
     ((and (list? pat) (not (null? pat)))
       
       (lets f (car pat)
         (case f
           ((quote)
-            (if (equal? (car (cdr pat)) val) ht #f))
+            (if (equal? (car (cdr pat)) val) '() #f))
           ((quasiquote)
-            (match-qp val (car (cdr pat)) ht))
+            (match-qp val (car (cdr pat)) env))
           ((list)
-            (if (list? val) (match-list val (cdr pat) ht match-pat) #f))
+            (if (list? val) (match-list val (cdr pat) env match-pat) #f))
           ((list-rest)
-            (if (pair? val) (match-list-rest val (cdr pat) ht) #f))
+            (if (pair? val) (match-list-rest val (cdr pat) env) #f))
           ((cons)
-            (if (pair? val) (match-cons val (cdr pat) ht) #f))
+            (if (pair? val) (match-cons val (cdr pat) env) #f))
           ((and)
             (do
-              ((p (cdr pat) (cdr p))
-               (ht (hash-table-copy ht)))
-              ((or (not (hash-table? ht))
-                   (null? p))
-               (if (and (hash-table? ht) (null? p)) ht #f))
-              (set! ht (match-pat val (car p) ht))))
+                ((p (cdr pat) (cdr p))
+                 (bind '()))
+                ((or (null? p)
+                     (not bind)) bind)
+              (lets res (match-pat val (car p) env)
+                (set! bind (and res (append res bind)))
+                (set! env (and res (append res env))))))
           ((or)
-            (do
-              ((p (cdr pat) (cdr p))
-                (h #f))
-              ((or (hash-table? h)
-                   (null? p))
-               (if (hash-table? h) h #f))
-              (set! h (match-pat val (car p) ht))))
+            (do 
+                ((p (cdr pat) (cdr p))
+                 (bind #f))
+                ((or (null? p)
+                     bind) bind)
+              (set! bind (match-pat val (car p) env))))
           ((not)
             (do
               ((p (cdr pat) (cdr p))
-               (ht (hash-table-copy ht))
-               (h #f))
-              ((or (hash-table? h)
-                   (null? p))
-               (if (hash-table? h) #f ht))
-              (set! h (match-pat val (car p) ht))))
-          (else #f))))
+               (bind #f))
+              ((or (null? p)
+                   bind) (and (not bind) '()))
+              (set! bind (match-pat val (car p) env))))
+          (else (error "Unrecognized pattern")))))
 
-    (else #f)))
+    (else (error "Unrecognized pattern"))))
       
 
 (def-d-fexpr match (expr . clauses)
   
     (let ((val (eval-prev expr))
-          (env (env-ref 1)))
+          (env (get-env-tail 1)))
 
       (with/cc return
         (do ((clauses clauses (cdr clauses)))
             ((null? clauses) #f)
             
-          (lets res (match-pat val (car (car clauses)) (make-hash-table))
-            (if (hash-table? res)
-              (return (eval-all (cdr (car clauses)) (cons res env)))))))))
-      
+          (lets res (match-pat val (car (car clauses)) '())
+            (if res
+              (return (eval-all (cdr (car clauses)) (append res env)))))))))
+ 
 ; These test cases are taken from 
 ; https://docs.racket-lang.org/reference/match.html
 
@@ -223,6 +224,12 @@
   20)
 
 (assert-equal
+  (match 5
+    ((not 4) 'yes))
+  'yes)
+
+
+(assert-equal
   (match '(1 2)
    ((or (list a 1) (list a 2)) a))
   1)
@@ -252,8 +259,6 @@
   (match '(1 2 3)
     (`(1 ,a 3) a))
   2)
-
-
 
 
 
