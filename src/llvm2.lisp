e1b892a4088ee8ff91aa293b50f3c7271db0e1b4
Léon van Velzen <leonvanvelzen@protonmail.com>
2020-08-30 11:59:20 +0200

    Add declare statement

--- src/llvm2.lisp
+++ src/llvm2.lisp
@@ -3,6 +3,14 @@
   (curry list ins))
 
 
+(def-d-fun type->string (t)
+  (cond
+    ((symbol? t)
+      (symbol->string t))
+    ((and (list? t) (equal? (car t) 'pointer))
+      (string-append (type->string (cadr t)) "*"))))
+      
+      
 (def-d-fun args->string (args)
   (lets as (map (d-fun (a)
                     (string-append "i32 " (any->string a)))
@@ -17,6 +25,7 @@
           (statement->string (cadr stmt))
           " = "
           (statement->string (caddr stmt))))
+      
       ((fun)
         (string-append
           "define i32 "
@@ -24,11 +33,18 @@
           (args->string (caddr stmt)) " {\n"
           (program->string (cdddr stmt)) 
           "\n}"))
+
       ((ret)
-        (string-append "ret " (statement->string (cadr stmt))))
+        (string-append "ret i32 " (statement->string (cadr stmt))))
+      
       ((call)
         (string-append "call i32 " (any->string (cadr stmt))
                                    (args->string (caddr stmt))))
+      ((declare)
+        (string-append "declare " (type->string (cadr stmt)) " "
+          (symbol->string (caddr stmt))
+          "(" (string-join ", " (map type->string (list-ref stmt 3))) ")"))
+          
       (else
         (string-join " " (map any->string stmt))))
     (any->string stmt)))
@@ -43,9 +59,40 @@
 (def-d-fun print-program (program)
   (print (program->string program)))
  
+(def-d-fun llvm-header. ()
+  '(declare i32 @printf ((pointer i32) ...)))
+
+(def-d-fun with-header. program
+  (cons
+    (llvm-header.)
+    program))
+    
 (print-program
   (list
-    (fun. 'times2 '(a)
-      '(ret (mult a 2)))
-    '(call times2 (10 20))
-    '(= x (call times2 (50)))))
+    (fun. '@main '()
+      '(ret 10))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
